2495df03373d9e9178c588c9739161ed48c716ef
mjhu <mjhu91@gmail.com>
2012-09-26 14:15:13 -0400

    setup

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function login() {
+    //TODO
+}
+
+function logout() {
+    //TODO
+}
+
+function focus() {
+    //TODO
+}
+
+function create() {
+    //TODO
+}
+
+function destroy() {
+    //TODO
+}
+
+function getTime() {
+    //TODO
+}
+
+function getSite() {
+    //TODO
+}
+
+function inactive() {
+    //TODO
+}
+
+function active() {
+    //TODO
+}
