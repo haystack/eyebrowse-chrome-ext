57bce6edcfe11c8daab15efdf0eda7843589190d
mjhu <mjhu91@gmail.com>
2012-09-28 16:58:39 -0400

    added method to remove pages visited

--- js/popup.js
+++ js/popup.js
@@ -4,4 +4,8 @@ $(document).ready(function() {
   for (var i =0; i < v.length; i++) {
     $('#main').append('<div>' + (i+1) + ". " + v[i] + '</div>');
   }
+  $('#reset').click(function (e) {
+     $('#main').html("Pages Visited:");
+     backpage.visited = [];
+  });
 });
