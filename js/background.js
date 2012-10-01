c7267b5231c07727a19b64fa3de1cc33adfd4a35
Joshua Blum <joshblum@mit.edu>
2012-10-01 18:47:23 -0400

    handler sckeleton in place

--- js/background.js
+++ js/background.js
@@ -3,24 +3,33 @@
 //When a new page is navigated to (must filter for bad urls)
 //When a new window is opened/selected (same as tab event)
 //When a tab or window is destroyed
+//API info: http://developer.chrome.com/extensions/tabs.html
+
+
+///////////////Event listeners/////////////// 
+//Specific to Chrome. Each calls the data processer function in main.js to be process and recorded if necessary. These functions should check things like incognito windows, the handler checks for things like restricted sites and user permissions that are set. This allows firefox to use the same main. 
+
 visited = []
+
+//Fires when the active tab in a window changes. Note that the tab's URL may not be set at the time this event fired, but you can listen to onUpdated events to be notified when a URL is set.
+//We need to listen for both (so we know when new tabs/windows appear). But no double counting. 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
     chrome.tabs.get(activeInfo.tabId, function (tab) {
-        visited.push(tab.url);
+        record_history(activeInfo.tabId, tab.url, tab.title)
     });
-    update_badge();
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     chrome.tabs.get(tabId, function (tab) {
+        record_history(tabId, tab.url, tab.title)
+    });
     
-            visited.push(tab.url);
-    
-        });
-    
-    update_badge();
 });
 
-
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    chrome.tabs.get(tabId, function (tab) {
+        record_history(tabId, tab.url, tab.title)
+    });
+});
 
 
--- js/main.js
+++ js/main.js
@@ -34,6 +34,19 @@ function active() {
     //TODO
 }
 
+visited = [] // tmp until we figure out local storage
+
+//inputs:
+//tabId - indentifer of tab (unique to session only)
+//url - url of the tab making the request
+//title - title of the webpage the tab is displaying
+//event_type - whether a tab is opening or closing/navigating to a new page etc
+function record_history(tabId, url, title, event_type) {
+    visited.push(url)
+    update_badge();
+}
+
+
 //tmp for dev
 function update_badge() {
 
