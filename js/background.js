399f6a57290c2bf2686731a377e4912cde08b132
Joshua Blum <joshblum@mit.edu>
2012-10-01 15:34:21 -0400

    spacing on js files

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@
 // React when a browser action's icon is clicked.
 visited = []
 chrome.tabs.onActivated.addListener(function(info) {
-  console.log(info.tabId);
-  chrome.tabs.get(info.tabId,function (tab) {console.log(tab.url);visited.push(tab.url);console.log(visited.length)})
-  chrome.browserAction.setBadgeText({text: (visited.length + 1) + ''});
+    console.log(info.tabId);
+    chrome.tabs.get(info.tabId,function (tab) {console.log(tab.url);visited.push(tab.url);console.log(visited.length)})
+    chrome.browserAction.setBadgeText({text: (visited.length + 1) + ''});
 });
--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  window.backpage = chrome.extension.getBackgroundPage();
-  var v = backpage.visited;
-  for (var i =0; i < v.length; i++) {
-    $('#main').append('<div>' + (i+1) + ". " + v[i] + '</div>');
-  }
-  $('#reset').click(function (e) {
-     $('#main').html("Pages Visited:");
-     backpage.visited = [];
-  });
+    window.backpage = chrome.extension.getBackgroundPage();
+    var v = backpage.visited;
+    for (var i =0; i < v.length; i++) {
+        $('#main').append('<div>' + (i+1) + ". " + v[i] + '</div>');
+    }
+    $('#reset').click(function (e) {
+        $('#main').html("Pages Visited:");
+        backpage.visited = [];
+    });
 });
