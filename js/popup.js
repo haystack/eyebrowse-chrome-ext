daad8d63bf5645be31827ae3c44b9e24734fecf0
Joshua Blum <joshblum@mit.edu>
2012-10-01 22:37:39 -0400

    fixed typo

--- js/popup.js
+++ js/popup.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     window.backpage = chrome.extension.getBackgroundPage();
     var items = backpage.open_items;
     $.each(items, function(key, value) {
-        $('.visited-items').append(sprintf('<li>%s, %s, %s, %s</li>', value.tabId, value.url, value.title, value.event_type));
+        $('.visited-items').append(sprintf('<li>%s, %s, %s, %s</li>', value.tabId, value.url, value.title, value.start_event));
         });
 
     $('#reset').click(function (e) {
