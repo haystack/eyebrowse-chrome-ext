11c6bccbade99431523cbac0adabdc738458956a
Joshua Blum <joshblum@mit.edu>
2012-10-31 22:13:45 -0400

    login to eyebrowse server

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,147 @@
+LoginView = Backbone.View.extend({
+    'el' : $('.content-container'),
+
+    initialize : function() {
+         _.bindAll(this);
+        this.render();
+    },
+
+    render : function() {
+        if (!user.isLoggedIn()) {
+            $('.content-container').empty();
+            $('body').css('width', '300px');
+            var template = _.template($("#login_template").html(), {
+                    'baseUrl' : baseUrl,
+                });
+
+            $(this.el).html(template);
+            $('#errors').fadeOut();
+            $('#id_username').focus();
+        }
+    },
+
+    events : {
+        "click #login" : "getLogin",
+        "keypress input" : "filterKey"
+    },
+
+    filterKey : function(e) {
+        if (e.which == 13) { // listen for enter event
+            e.preventDefault();
+            this.getLogin()
+        }
+    },
+
+    getLogin : function() {
+        $('#errors').fadeOut();
+        var self = this;
+        var username = $('#id_username').val();
+        var password = $('#id_password').val();
+        if (username === '' || password === '') {
+            self.displayErrors("Enter a username and a password")
+        } else {
+            $.get(url_login(), function(data) {
+                self.postLogin(data, username, password);
+            });
+        }
+    },
+
+    postLogin : function(data, username, password) {
+        var REGEX = /name\='csrfmiddlewaretoken' value\='.*'/; //regex to find the csrf token
+        var match = data.match(REGEX);
+        var self = this;
+        if (match) {
+            match = match[0]
+            var csrfmiddlewaretoken = match.slice(match.indexOf("value=") + 7, match.length-1); // grab the csrf token
+            //now call the server and login
+            $.ajax({
+                url: url_login(),
+                type: "POST",
+                data: {
+                        "username": username,
+                        "password": password,
+                        "csrfmiddlewaretoken" : csrfmiddlewaretoken,
+                        "remember_me": 'on', // for convience
+                },
+                dataType: "html",
+                success: function(data) {
+                    var match = data.match(REGEX)
+                    if(match) { // we didn't log in successfully
+                        self.displayErrors("Invalid username or password");
+                    } else {
+                        self.completeLogin()
+                    }
+                },
+                error : function(data) {
+                    self.displayErrors("Unable to connect, try again later.")
+                }
+            });
+        }
+        else {
+            self.completeLogin();
+        }
+    },
+
+    completeLogin : function() {
+        $('#login_container').remove();
+        $('body').css('width', '600px');
+        user.setLogin(true);
+        navView.render('home_tab');
+        subNavView.render();
+    },
+
+    logout : function() {
+        $.get(url_logout());
+        user.setLogin(false);
+        this.render();
+    },
+
+    displayErrors : function(errorMsg) {
+        $errorDiv = $('#errors');
+        $errorDiv.html(errorMsg);
+        $errorDiv.fadeIn();
+    },
+
+});
+
+NavView = Backbone.View.extend({
+    'el' : $('.nav-container'),
+
+    initialize : function(){
+        this.render('home_tab');
+        $('.brand').blur()
+    },
+
+    render : function(tab) {
+        $('.nav-container').empty();
+        var loggedIn = user.isLoggedIn();
+        var template = _.template($("#nav_template").html(), {
+                baseUrl : baseUrl,
+                loggedIn : loggedIn,
+            });
+
+        $(this.el).html(template);
+        if (!loggedIn) {
+            tab = "login_tab"
+        }
+        $('nav-tab').removeClass('active');
+        $('#' + tab).addClass('active');
+    },
+});
+
+///////////////////URL BUILDERS///////////////////
+function url_login() {
+    return baseUrl + '/accounts/login/'
+}
+
+function url_logout() {
+    return baseUrl + '/accounts/logout/'
+}
+
 $(document).ready(function() {
     window.backpage = chrome.extension.getBackgroundPage();
-    var items = backpage.local_storage;
-    $.each(items, function(key, value) {
-        $('.visited-items').append(sprintf('<li>%s, %s, %s, %s</li>', value.tabId, value.url, value.title, value.start_event));
-        });
-
-    $('#reset').click(function (e) {
-        $('#main').html("Pages Visited:");
-        backpage.local_storage = [];
-    });
+    user = backpage.user;
+    baseUrl = backpage.baseUrl;
+    navView =  new NavView();
+    loginView = new LoginView();
 });
