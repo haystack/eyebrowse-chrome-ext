f643b65827fc8e75ed110c8b0630559180c8c969
Scott Greenwald <swgreen@mit.edu>
2012-12-01 21:49:31 -0500

    options page now shows localStorage user info

--- js/options.js
+++ js/options.js
@@ -1,33 +1,20 @@
-//$(document).ready(function() {
-//    $("#main").html("Hello World Options Page 2345"+localStorage.getItem("serverURL"));
-//});
-
 $(document).ready(function() {
 
-    var myDiv = $(document.createElement("div"));
-    $('#options').append(myDiv);
+    var myDiv = $('#myDiv');
     
     // SERVER ADDRESS GET/SET
-    //  next: save change to localStorage
-    //   will it be a problem to get persistence across sessions? 
 
     var urlBox = $(document.createElement("input"));
-    urlBox.val(localStorage.getItem("serverURL"));
+    urlBox.val(localStorage.getItem("baseUrl"));
     urlBox.width(500);
     var urlBoxLabel = $(document.createElement("span"));
     urlBoxLabel.text("Server URL ");
     myDiv.append(urlBoxLabel);
     myDiv.append(urlBox);
 
-    myDiv.append($(document.createElement("p")));
-    myDiv.append($(document.createElement("span")).text("To set, press enter to in the textbox."));
-    myDiv.append($(document.createElement("br")));    
-    myDiv.append($(document.createElement("span")).text("To disable, delete the url and preset enter."));
-    myDiv.append($(document.createElement("br")));
-
     urlBox.bind("enterKey",function(e){
            console.log("enter key in urlbox");//do stuff here
-           localStorage.setItem("serverURL",urlBox.val()); // is there something more like "self"?
+           localStorage.setItem("baseUrl",urlBox.val()); // is there something more like "self"?
     });
     urlBox.keyup(function(e){
         if(e.keyCode == 13)    {
@@ -35,18 +22,38 @@ $(document).ready(function() {
         }
     });
 
-    // BLACKLIST LISTING
-    var blacklistLabel = $(document.createElement("span"));
-    blacklistLabel.append($(document.createElement("h2")).text("Blacklist"));
-    myDiv.append(blacklistLabel);
-    var blacklistUL = $(document.createElement("ul"));
-    myDiv.append(blacklistUL);
-    window.backpage = chrome.extension.getBackgroundPage();
-    var blacklist = backpage.user.getBlackList().getSet();
-    console.log(blacklist);
-    $.each(blacklist, function(key, value) {
-        blacklistUL.append($(document.createElement("li")).text(value));
-        console.log($(document.createElement("li")).text(value));
+    var console = chrome.extension.getBackgroundPage().console;
+    console.log("Hello world from options.js");
+
+    // Read user info from localStorage
+    var userInfo = $('#userInfo');
+    var userVal = JSON.parse(localStorage.getItem("user"));
+
+    // auxiliary function for listing properties
+    var populateDivWithList = function(someDiv, list, property) {
+        $.each(list, function(key, value) {
+            someDiv.append($('<div></div>').text(value[property]));
         });
-    var blacklistUL = $(document.createElement("ul"));
+    };
+
+    //
+    // Populate whitelist 
+    //
+
+    var wlDiv = $('#whitelist');
+    populateDivWithList(wlDiv,userVal.whitelist,'url');
+
+    //
+    // Populate blacklist
+    //
+
+    var blDiv = $('#blacklist');
+    populateDivWithList(blDiv,userVal.blacklist, 'url');
+
+    var liDiv = $('#loggedIn');
+    liDiv.append($('<div></div>').text(userVal.username+", "+userVal.loggedIn));
+
+    var rjDiv = $('#rawJson');
+    rjDiv.append($('<div></div>').text(JSON.stringify(userVal)));
+
 });
