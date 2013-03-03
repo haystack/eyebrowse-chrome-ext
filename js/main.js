aacafe4afc71c72899f515a78c849ba9118320a3
Joshua Blum <joshblum@mit.edu>
2013-03-02 22:59:23 -0500

    main

--- js/main.js
+++ js/main.js
@@ -4,6 +4,168 @@ var baseUrl = "http://localhost:5000";
 //var baseUrl = "http://eyebrowse.herokuapp.com"
 var siteName = "Eyebrowse";
 
+///////////////////models//////////////////////
+
+//This object can represent either a whitelist or blacklist for a given user. On an update send results to server to update stored data. On intialization set is synced with server. Should allow offline syncing in the future.
+var FilterListItem = Backbone.Model.extend({
+    parse: function(data) {
+        if (data !== null) {
+            return {
+                url : data.url, 
+                id : data.id,
+            }
+        }
+    },
+});
+
+
+var FilterList = Backbone.Collection.extend({
+
+    model: FilterListItem,
+
+    initialize: function(type) {
+        _.bindAll(this);
+        this.type = type;
+        this._fetch();
+    },
+
+    getType : function() {
+        return this.get("type")
+    },
+
+    url : function() {
+        return getApiURL(this.type)
+    },
+
+    parse: function(data, res){
+        if (res.status === 200) {
+            return data.objects;    
+        }
+    },
+
+    //wrapper for fetch which logs user out if server errs
+    _fetch: function() {
+        this.fetch({
+            error: _.bind(function(model, xhr, options) {
+                user.logout();
+            }, this)
+        });
+    },
+});
+
+
+//User object holds the status of the user, the cookie from the server, preferences for eyebrowse, whitelist, blacklist, etc
+var User = Backbone.Model.extend({
+    defaults: {
+        "loggedIn" : false,
+        "whitelist" : new FilterList("whitelist"),
+        "blacklist" : new FilterList("blacklist"),
+        "username" : "",
+        "resourceURI" : "/api/v1/user/",
+    },
+
+    initialize : function() {
+        _.bindAll(this); //allow access to 'this' in callbacks with "this" meaning the object not the context of the callback
+
+    },
+
+    getWhitelist : function() {
+        return this.get("whitelist")
+    },
+
+    getBlacklist : function() {
+        return this.get("blacklist")
+    },
+
+    getUsername : function() {
+        return this.get("username")
+    },
+
+    getResourceURI : function() {
+        return this.get("resourceURI")
+    },
+
+    isLoggedIn : function() {
+        if (this.getUsername() === this.defaults.username || this.getResourceURI() === this.defaults.resourceURI) {
+            this.logout();
+        }
+        return this.get("loggedIn")
+    },
+
+    //when the user is logged in set the boolean to give logged in views.
+    setLogin : function(status) {
+        this.set({ 
+            "loggedIn": status,
+        });
+
+        var map = {
+            "true" : "login",
+            "false" : "logout"
+        };
+
+        loginBadge(map[status]);
+    },
+
+    login : function() {
+        this.setLogin(true);
+    },
+
+    logout : function() {
+        this.setLogin(false);
+    },
+    
+    setUsername : function(username) {
+        this.set({ 
+            "username": username,
+        });
+        this.setResourceURI(username);
+    },
+
+    setResourceURI : function(username) {
+        this.set({
+            "resourceURI" : sprintf("/api/v1/user/%s/", username)
+        })
+    },
+
+    setWhitelist : function(whitelist) {
+        this.setFilterSet("whitelist", whitelist);
+    },
+
+    setBlacklist : function(blacklist) {
+        this.setFilterSet("blacklist", blacklist);
+    },
+
+    setFilterSet : function(type, list) {
+        this.set({
+            type : list
+        })
+    },
+
+    //check if a url is in the blacklist
+    inBlackList : function(url) {
+        return this.inSet("blacklist", url)
+    },
+
+    //check if a url is in the whitelise
+    inWhitelist : function(url) {
+        return this.inSet("whitelist", url)
+    },
+
+    //check if url is in a set (either whitelist or blacklist)
+    // documentation for URL.js : http://medialize.github.com/URI.js/docs.html
+    inSet : function(setType, url) {
+        var set = this.get(setType);
+        var uri = new URI(url)
+        var hostname = uri.hostname();
+        var protocol = uri.protocol();
+        return (set.where({"url" : hostname}).length || set.where({"url" : protocol}).length || set.where(url).length)
+    },
+
+    //save the current state to local storage
+    saveState : function(){
+        localStorage.user = JSON.stringify(this);
+    },
+});
 
 
 //////////////////data collection handlers/////////////
@@ -190,12 +352,12 @@ function serializePayload(payload) {
 function getApiURL(resource, id, params) {
     params = params || {};
     var apiBase = sprintf("%s/api/v1/%s", baseUrl, resource);
-    var getParams = "
+    var getParams = ""
     for (var key in params) {
       getParams += sprintf("&%s=%s", key, params[key]);
     }
     
-    if (getParams !== ") {
+    if (getParams !== "") {
         apiBase += "?" + getParams.slice(1);
     }
     if (id != null) {
@@ -213,7 +375,7 @@ function openLink(url) {
 
 ///////////////////local storage methods//////////////
 function loadLocalHistory() {
-    localString = localStorage.local_history/;
+    localString = localStorage.local_history;
     localString = (localString) ? localString : "[]"; // catch undefined case
     return JSON.parse(localString);
 }
@@ -279,7 +441,7 @@ function loginBadge(e) {
     if (e == "logout") {
         updateBadge("!");
     } else if(e == "login") {
-        updateBadge(");
+        updateBadge("");
     }
 }
 
@@ -290,15 +452,12 @@ function initBadge() {
     }
 }
 
+// dictionary mapping all open items. Keyed on tabIds and containing all information to be written to the log. 
+var activeItem;
 
-$(document).ready(function(){
-    // dictionary mapping all open items. Keyed on tabIds and containing all information to be written to the log. 
-    var activeItem;
+local_history = loadLocalHistory();
 
-    local_history = loadLocalHistory();
+user = getLocalStorageUser();
+initBadge()
 
-    user = getLocalStorageUser();
-    initBadge()
-
-    localSetIfNull("baseUrl", baseUrl);  
-});
+localSetIfNull("baseUrl", baseUrl);
