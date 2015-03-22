82ed27c042d9b184b54c0ceb8e545f2ada776cb4
Joshua Blum <joshblum@mit.edu>
2015-03-22 22:59:06 +0200

    Fix problem with URIs

--- js/main.js
+++ js/main.js
@@ -914,29 +914,6 @@ function isActiveTab(tabArray, tabId) {
     return activeTab !== null && activeTab.id === tabId;
 }
 
-/*
- * Parse urls
- * https://gist.github.com/jlong/2428561
- */
-function getUrlParser(url) {
-    var parser = document.createElement('a');
-    parser.href = url;
-    return parser;
-}
-
-function URI(url) {
-    var parser = document.createElement('a');
-    parser.href = url;
-    this.protocol = parser.protocol; // => "http:"
-    this.hostname = parser.hostname; // => "example.com"
-    this.port = parser.port; // => "3000"
-    this.pathname = parser.pathname; // => "/pathname/"
-    this.search = parser.search; // => "?search=test"
-    this.hash = parser.hash; // => "#hash"
-    this.host = parser.host; // => "example.com:3000"
-    parser.remove();
-}
-
 // dictionary mapping all open items. Keyed on tabIds and containing all
 // information to be written to the log.
 var activeItem;
---
+++ libs/URI.js
@@ -0,0 +1,23 @@
+/*
+ * Parse urls
+ * https://gist.github.com/jlong/2428561
+ */
+function getUrlParser(url) {
+    var parser = document.createElement('a');
+    parser.href = url;
+    return parser;
+}
+
+function URI(url) {
+    var parser = document.createElement('a');
+    parser.href = url;
+    this.protocol = parser.protocol; // => "http:"
+    this.hostname = parser.hostname; // => "example.com"
+    this.port = parser.port; // => "3000"
+    this.pathname = parser.pathname; // => "/pathname/"
+    this.search = parser.search; // => "?search=test"
+    this.hash = parser.hash; // => "#hash"
+    this.host = parser.host; // => "example.com:3000"
+    parser.remove();
+}
+
