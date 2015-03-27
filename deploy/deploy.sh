4623d6dd1f9c4a5523a9c2465c703f85239131cc
Joshua Blum <joshblum@mit.edu>
2015-03-28 02:11:26 +0300

    push origin master when deploying

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,9 +7,9 @@ version=`python deploy/deploy.py`
 git add manifest.json
 git commit -m "Webstore deploy $version"
 git checkout js/common.js
+git push origin master
 
 # remove old zip and add new one
 cd ../
 rm eyebrowse-chrome-ext.zip
 zip -r eyebrowse-chrome-ext.zip eyebrowse-chrome-ext/ -x *.git*
-
