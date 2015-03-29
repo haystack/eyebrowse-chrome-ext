e8e5e43adb0df9d24a22dcd57c4ceef53080d8ba
Joshua Blum <joshblum@mit.edu>
2015-03-29 09:14:56 +0300

    fix deploy script, baseURL bug

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,14 +2,16 @@
 
 # make baseUrl production url, update the manifest count
 version=`python deploy/deploy.py`
-
-# clean git
-git add manifest.json
-git commit -m "Webstore deploy $version"
-git checkout js/common.js
-git push origin master
+ROOT=`pwd`
 
 # remove old zip and add new one
 cd ../
 rm eyebrowse-chrome-ext.zip
 zip -r eyebrowse-chrome-ext.zip eyebrowse-chrome-ext/ -x *.git*
+
+# clean git
+cd $ROOT
+git add manifest.json
+git commit -m "Webstore deploy $version"
+git checkout js/common.js
+git push origin master
