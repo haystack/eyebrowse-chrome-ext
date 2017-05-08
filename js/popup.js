1ad2b903b4e8c5cdf32758608b2e03c433b5d602
Jessica <jessizici@gmail.com>
2017-05-08 03:18:12 -0400

    Fb SDK

--- js/popup.js
+++ js/popup.js
@@ -69,7 +69,7 @@ window.fbAsyncInit = function() {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) {return;}
     js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    js.src = "../js/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
